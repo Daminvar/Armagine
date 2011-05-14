b279d110458b8b78eaeb3824b06ffb47ac364f27
unknown <IGMAdmin@.(none)>
2011-05-14 18:47:03 -0400

    Resized helpscreen

--- Armagine/Assets/Menu_Assets/HelpScript.js
+++ Armagine/Assets/Menu_Assets/HelpScript.js
@@ -2,7 +2,7 @@ var helpBox : GUIStyle;
 
 function OnGUI () {
 	// Make a background box
-	GUI.Box (Rect ((Screen.width/2)-350,(Screen.height/2)-350,700,700), "Let me give you a hand...", helpBox);	
+	GUI.Box (Rect (0,0,Screen.width,Screen.height), "", helpBox);	
 
 	// Make the menu button.
 	if (GUI.Button (Rect ((Screen.width/2)-300,(Screen.height/2)+210,100,30), "<- Menu")) {
